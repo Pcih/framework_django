fe9ed6e356387f3da00f6b203931df9f44c72d13
pcih <pcih1984@gmail.com>
2023-11-09 00:15:58 +0300

    fir 2

--- lesson_2/admin.py
+++ lesson_2/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Order, Product
 
-# Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Product)
